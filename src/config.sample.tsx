636bc83fc966c37c78749d8aeb426263e5c99d5f
CLOUDERHEM <cloudac@qq.com>
2023-04-19 12:59:34 +0800

    add config demo

---
+++ src/config.sample.tsx
@@ -0,0 +1,10 @@
+const config = {
+    appId: "xxx",
+    appKey: "xxx",
+    serverURL: "https://xxx",
+    maxWordLen: 20,
+    waitTimeAfterTrans: 2000,
+    storageClassName: "Record"
+};
+
+export default config;
