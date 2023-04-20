2d4b2ddedadbefbadcc0626e515783d61f3829e1
CLOUDERHEM <cloudac@qq.com>
2023-04-20 19:27:42 +0800

    delete useless import

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,8 +1,6 @@
 import App from "./App";
-import "moment/locale/zh-cn";
 import "antd/dist/antd.css";
 import React from "react";
-
-const IndexPopup: React.FC = () => <App />;
-
-export default IndexPopup;
+export default function(){
+    return <App/>
+};
